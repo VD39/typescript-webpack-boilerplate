d025bb782ec6a4dd73e3af5f3b7e154088a07489
VD39 <VD39@users.noreply.github.com>
2017-07-20 18:10:51 +0200

    Updated to include Browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 const config = {
   entry: path.join(__dirname, '/app/index'),
@@ -11,40 +13,49 @@ const config = {
     contentBase: path.join(__dirname, '/public/')
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader', 'eslint-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=1000000'
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'raw-loader'
-      }
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['babel-loader', 'eslint-loader']
+    },
+    {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'postcss-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=1000000'
+    },
+    {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'postcss-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.html$/,
+      exclude: /node_modules/,
+      loader: 'raw-loader'
+    }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('assets/css/styles.css')
+    new ExtractTextPlugin('assets/css/styles.css'),
+    new BrowserSyncPlugin(
+      {
+        host: 'localhost',
+        port: 9000,
+        proxy: 'http://localhost:9100/'
+      },
+      {
+        reload: false
+      }
+    )
   ],
   watch: false
 };
